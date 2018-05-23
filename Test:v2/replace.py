03148e194569785fe602f705b09f32b06ce46278
Thanos X <spareme@mail.com>
2018-05-23 16:38:47 +0300

    Python script to change the port of envoy config

---
+++ Test:v2/replace.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import ruamel.yaml
+import os
+
+file_name = 'service-envoy.yaml'
+from ruamel.yaml.util import load_yaml_guess_indent
+
+config, ind, bsi = load_yaml_guess_indent(open(file_name))
+
+static_resources = config['static_resources']
+static_resources['clusters'][0]['hosts'][0]['socket_address']['port_value'] = os.env$
+#instances[0]['username'] = 'Username'
+#instances[0]['password'] = 'Password'
+
+ruamel.yaml.round_trip_dump(config, open('output.yaml', 'w'), 
+                            indent=ind, block_seq_indent=bsi)
+
+
+
+
+
+
