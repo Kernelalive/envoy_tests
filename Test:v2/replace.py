86e7de82d59a256682f752455278a3502b074bb2
Thanos X <spareme@mail.com>
2018-05-24 12:04:55 +0300

    Custom Script intagration

--- Test:v2/replace.py
+++ Test:v2/replace.py
@@ -3,13 +3,13 @@
 import ruamel.yaml
 import os
 
-file_name = 'service-envoy.yaml'
+file_name = '/etc/service-envoy.yaml'
 from ruamel.yaml.util import load_yaml_guess_indent
 
 config, ind, bsi = load_yaml_guess_indent(open(file_name))
 
 static_resources = config['static_resources']
-static_resources['clusters'][0]['hosts'][0]['socket_address']['port_value'] = os.env$
+static_resources['clusters'][0]['hosts'][0]['socket_address']['port_value'] = os.environ['PORT_V']
 #instances[0]['username'] = 'Username'
 #instances[0]['password'] = 'Password'
 
--- Test:v2/start_service.sh
+++ Test:v2/start_service.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 python3 /code/service.py &
-envoy -c /etc/service-envoy.yaml --v2-config-only
+envoy -c /code/output.yaml --v2-config-only
